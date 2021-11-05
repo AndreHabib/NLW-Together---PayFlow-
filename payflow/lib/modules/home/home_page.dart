276e67d05139fde58abbebff3ed0985f0effdb10
AndreHabib <andre.habib.017@ufrn.edu.br>
2021-11-05 11:29:36 -0300

    first version of the payflow project

---
+++ payflow/lib/modules/home/home_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_controller.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final homeController = HomeController();
+  final pages = [
+    Container(color: Colors.red),
+    Container(color: Colors.blue),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                    text: 'Olá, ',
+                    style: TextStyles.titleRegular,
+                    children: [
+                      TextSpan(
+                          text: "André", style: TextStyles.titleBoldBackground)
+                    ]),
+              ),
+              subtitle: Text('Mantenha suas contas em dia',
+                  style: TextStyles.captionShape),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: pages[homeController.currentPage],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+                onPressed: () {
+                  homeController.setPage(0);
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.home,
+                  color: AppColors.primary,
+                )),
+            InkWell(
+              onTap: () => Navigator.pushNamed(context, '/barcode_scanner'),
+              child: Container(
+                height: 56,
+                width: 56,
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                homeController.setPage(1);
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.description_outlined,
+                color: AppColors.body,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
